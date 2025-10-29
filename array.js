e3757ac1014cc1ca4c9adb616fd57725e450417f
haront7002-sketch <haron.t.7002@gmail.com>
2025-10-29 14:47:59 +0200

    Add files via upload

---
+++ array.js
@@ -0,0 +1,16 @@
+let colors = [];
+
+colors.push("pink");
+colors.push("purple");
+colors.push("yellow");
+
+let numbers = [10, 20, 30, 40, 50];
+
+colors.pop();
+
+colors.unshift("red");
+
+console.log("Colors array:", colors);
+console.log("Numbers array:", numbers);
+console.log("Length of colors array:", colors.length);
+console.log("Length of numbers array:", numbers.length);
